58912c86f49d8ff1e9648468803d3a89aff8aacd
Popular Belbase <you@example.com>
2022-04-08 18:43:56 +0200

    setup heroku foundation and deployment

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+# posts/urls.py
+from unicodedata import name
+from django.urls import path
+from .views import HomePageView
+
+urlpatterns = [
+    path("" , HomePageView.as_view(), name="home"),
+]
