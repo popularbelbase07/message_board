30ed45a27d26b3a97dd2948f5517d6d1afa8c13e
Popular Belbase <you@example.com>
2022-04-08 18:43:11 +0200

    Automatic configuration with django-heroku

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
+from pyexpat import model
+from re import template
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Post
+    template_name = "home.html"
